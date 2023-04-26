3ec436f09aa8bb0b6f9a93632534c2beaff5dbe8
simon-1337 <simon.besenbaeck@gmail.com>
2023-04-26 15:09:55 +0200

    integrated firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'slack-clone-39542',
+    appId: '1:220704097275:web:a93c27b69fb02e5f66415e',
+    storageBucket: 'slack-clone-39542.appspot.com',
+    apiKey: 'AIzaSyCjXUMSRLNt44LCgNSD4bCnXh8uTQZ9eOA',
+    authDomain: 'slack-clone-39542.firebaseapp.com',
+    messagingSenderId: '220704097275',
+  },};
