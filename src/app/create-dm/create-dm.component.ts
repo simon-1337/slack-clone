275306a169949da9c9ad6ca07ffe99ae8ce80c9d
simon-1337 <simon.besenbaeck@gmail.com>
2023-05-31 14:17:18 +0200

    dms in sidenav

--- src/app/create-dm/create-dm.component.ts
+++ src/app/create-dm/create-dm.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CollectionReference, DocumentData, DocumentReference, Firestore, addDoc, collection, collectionData, doc, docData } from '@angular/fire/firestore';
+import { CollectionReference, DocumentData, DocumentReference, Firestore, addDoc, collection, collectionData, doc, docData, getDocs, query, where } from '@angular/fire/firestore';
 import { AuthService } from '../shared/auth.service';
 import { Observable } from 'rxjs';
 import { User } from 'src/models/user.class';
@@ -11,11 +11,11 @@ import { Router } from '@angular/router';
   templateUrl: './create-dm.component.html',
   styleUrls: ['./create-dm.component.scss']
 })
-export class CreateDmComponent implements OnInit{
+export class CreateDmComponent implements OnInit {
 
   private userColl: CollectionReference<DocumentData>;
   users$: Observable<any>;
-  allUsers: { id: string, name: string, mail: string, password: string, profileImageUrl: string}[] = [];
+  allUsers: { id: string, name: string, mail: string, password: string, profileImageUrl: string }[] = [];
 
   currentUser: User;
   userRef: any;
@@ -25,6 +25,7 @@ export class CreateDmComponent implements OnInit{
   receivingRef: any;
 
   chat: Dm = new Dm;
+  existingChatId: string;
 
   constructor(private firestore: Firestore, private auth: AuthService, private router: Router) {
     this.userColl = collection(this.firestore, 'users');
@@ -37,7 +38,7 @@ export class CreateDmComponent implements OnInit{
 
   getUsers() {
     this.users$ = collectionData(this.userColl, { idField: 'id' })
-    this.users$.subscribe( change => {
+    this.users$.subscribe(change => {
       this.allUsers = change;
     });
   }
@@ -48,25 +49,42 @@ export class CreateDmComponent implements OnInit{
     this.userRef = doc(this.userColl, this.userId);
     this.userData$ = docData(this.userRef);
     this.userData$.subscribe(data => {
-       this.currentUser = data;     
+      this.currentUser = data;
     });
   }
 
-  startChat(recipientUserId: string) {
-    this.fillDmObject(recipientUserId);
-    this.addDmDocumentToCurrentUser()
-      .then((docRef) => {
-        const documentId = docRef.id;
-        this.addDmDocumentToReceivingUser(recipientUserId);
-        this.redirectToChat(documentId);
-      });
+  async startChat(recipientUserId: string) {
+    const exists = await this.checkExistingChat(recipientUserId)
+    if (exists) {
+      this.redirectToChat(this.existingChatId);
+    } else {
+      this.fillDmObject(recipientUserId);
+      this.addDmDocumentToCurrentUser()
+        .then((docRef) => {
+          const documentId = docRef.id;
+          this.addDmDocumentToReceivingUser(recipientUserId);
+          this.redirectToChat(documentId);
+        });
+    }
   }
-  
+
+  async checkExistingChat(recipientUserId: string) {
+    const currentUserDms = collection(this.userRef, 'dms');
+    const dmQuery = query(currentUserDms, where('participants', 'array-contains', recipientUserId));
+    const querySnapshot = await getDocs(dmQuery); 
+    if (!querySnapshot.empty) {
+      console.log(querySnapshot.size);
+      this.existingChatId = querySnapshot.docs[0].id;
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   fillDmObject(recipientUserId: string) {
     this.chat.participants = [this.userId, recipientUserId];
   }
-  
+
   addDmDocumentToCurrentUser(): Promise<DocumentReference<DocumentData>> {
     return new Promise((resolve, reject) => {
       const dmColl = collection(this.userRef, 'dms');
